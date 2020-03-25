31b8b85f6d682dc8d7f139bed13d928fbd25e967
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-03-25 18:44:10 -0300

    newincident(api integration)

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,11 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
 import "./styles.css";
 
 import logo from "../../assets/logo.svg";
 
 export default function NewIncident() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+
+  const history = useHistory();
+  const ongId = localStorage.getItem("ongId");
+
+  const handleNewIncident = async event => {
+    event.preventDefault();
+    const data = {
+      title,
+      description,
+      value
+    };
+    data.value = value.replace(",", ".");
+    try {
+      await api.post("incidents", data, {
+        headers: {
+          authorization: ongId
+        }
+      });
+      history.push("/profile");
+    } catch (err) {
+      alert("Erro, tente novamente.");
+    }
+  };
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -18,14 +46,25 @@ export default function NewIncident() {
           </p>
           <Link className="back-link" to="/profile">
             <FiArrowLeft size={16} color="#E02041" />
-            Voltar para home
+            Voltar para início
           </Link>
         </section>
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="Valor em reais" />
-
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Título do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          ></textarea>
+          <input
+            placeholder="Valor em reais"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
           <button className="button" type="submit">
             Cadastrar
           </button>
